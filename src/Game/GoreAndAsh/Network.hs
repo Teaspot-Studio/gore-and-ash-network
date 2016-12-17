05bd1006839b9a6f6a6e9df62827ac03866a7deb
Anton Gushcha <ncrashed@gmail.com>
2016-12-18 02:46:32 +0300

    Fix reexports

--- src/Game/GoreAndAsh/Network.hs
+++ src/Game/GoreAndAsh/Network.hs
@@ -12,41 +12,6 @@ The core module contains API for basic networking for Gore&Ash. The network modu
 over Enet library, UDP transport with custom implementation of reliability. The API provides
 connection handling and basic message handling (bytestring sending and receiving).
 
-The module depends on following core modules:
-
-* logging - "Game.GoreAndAsh.Logging"
-
-So 'NetworkT' should be placed after 'LoggingT' in monad stack.
-
-The module is NOT pure within first phase (see 'ModuleStack' docs), therefore currently only 'IO' end monad can handler the module.
-
-Example of embedding:
-
-@
--- | Application monad is monad stack build from given list of modules over base monad (IO)
-type AppStack = ModuleStack [LoggingT, NetworkT, ... other modules ... ] IO
-newtype AppState = AppState (ModuleState AppStack)
-  deriving (Generic)
-
-instance NFData AppState
-
--- | Wrapper around type family
-newtype AppMonad a = AppMonad (AppStack a)
-  deriving (Functor, Applicative, Monad, MonadFix, MonadIO, MonadThrow, MonadCatch, LoggingMonad, NetworkMonad, ... other modules monads ... )
-
-instance GameModule AppMonad AppState where
-  type ModuleState AppMonad = AppState
-  runModule (AppMonad m) (AppState s) = do
-    (a, s') <- runModule m s
-    return (a, AppState s')
-  newModuleState = AppState <$> newModuleState
-  withModule _ = withModule (Proxy :: Proxy AppStack)
-  cleanupModule (AppState s) = cleanupModule s
-
--- | Arrow that is build over the monad stack
-type AppWire a b = GameWire AppMonad a b
-@
-
 -}
 module Game.GoreAndAsh.Network(
   -- * Types
@@ -61,6 +26,7 @@ module Game.GoreAndAsh.Network(
   , networkIncomingBandwidth
   , networkOutcomingBandwidth
   , networkDetailedLogging
+  , networkPollTimeout
   , networkNextOptions
   -- ** Errors
   , NetworkError(..)
